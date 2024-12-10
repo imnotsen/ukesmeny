da0c9f2fce53a7d48dc9056def3d0198a8435ae6
Sebastian Ness <sebastian.ness@gmail.com>
2024-12-10 16:36:51 +0100

    fixed local stuff

--- utils/supabase/supabase.ts
+++ utils/supabase/supabase.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://gtgcbceimujaoutvjpdw.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey!)
+export const supabase = createClient(supabaseUrl!, supabaseKey!)
