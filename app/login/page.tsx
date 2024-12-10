3af78b61202c61f2717fe5b103d05c3a94ac44b4
Sebastian Ness <sebastian.ness@gmail.com>
2024-12-10 15:33:37 +0100

    removed formAction, set type instead

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,13 +3,19 @@ import { Input } from "@/components/ui/input";
 import { login } from "./actions";
 
 export default function LoginPage() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    await login(formData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="email">Email:</label>
       <Input id="email" name="email" type="email" required />
       <label htmlFor="password">Password:</label>
       <Input id="password" name="password" type="password" required />
-      <Button formAction={login}>Log in</Button>
+      <Button type="submit">Log in</Button>
     </form>
   );
 }
